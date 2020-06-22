e0d43172733d5ea3f29b9e564b56c4c5138548c4
Alejandro Salazar Chiquito <ajsalazar4@espe.edu.ec>
2020-06-22 14:14:12 -0500

    Examen

---
+++ SlnBiblioteca/BEUBiblioteca/Transactions/LibroBLL.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUBiblioteca.Transactions
+{
+    public class LibroBLL
+    {
+
+        public static void Create(Libro a)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Libroes.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static Libro Get(int? id)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Libroes.Find(id);
+        }
+
+        public static void Update(Libro libro)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Libroes.Attach(libro);
+                        db.Entry(libro).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Libro libro = db.Libroes.Find(id);
+                        db.Entry(libro).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Libro> List()
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Libroes.ToList();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------
+        private static List<Libro> GetLibros(string criterio)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Libroes.Where(x => x.titulo.ToLower().Contains(criterio)).ToList();
+        }
+
+        private static Libro GetLibro(string titulo)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Libroes.FirstOrDefault(x => x.titulo == titulo);
+        }
+
+    }
+}
---
+++ SlnBiblioteca/BEUBiblioteca/Transactions/VideoBLL.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUBiblioteca.Transactions
+{
+    public class VideoBLL
+    {
+
+        public static void Create(Video a)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Videos.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static Video Get(int? id)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Videos.Find(id);
+        }
+
+        public static void Update(Video video)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Videos.Attach(video);
+                        db.Entry(video).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Video video = db.Videos.Find(id);
+                        db.Entry(video).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Video> List()
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Videos.ToList();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------
+        private static List<Video> GetVideos(string criterio)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Videos.Where(x => x.titulo.ToLower().Contains(criterio)).ToList();
+        }
+
+        private static Video GetVideo(string titulo)
+        {
+            DBBIBLIOTECAEntities db = new DBBIBLIOTECAEntities();
+            return db.Videos.FirstOrDefault(x => x.titulo == titulo);
+        }
+
+    }
+}
